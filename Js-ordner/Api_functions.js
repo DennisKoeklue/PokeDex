211dca0a0c3dc439dbdba0d35324e6ff5cc29b01
Dennis Köklü <dennis.koeklue@gmail.com>
2025-08-18 18:00:07 +0200

    added API and Function for load API Data

---
+++ Js-ordner/Api_functions.js
@@ -0,0 +1,39 @@
+const TYPE_COLORS = {
+      grass: "#78C850", fire: "#F08030", water: "#6890F0", bug: "#A8B820", normal: "#A8A878",
+      poison: "#A040A0", electric: "#F8D030", ground: "#E0C068", fairy: "#EE99AC", fighting: "#C03028",
+      psychic: "#F85888", rock: "#B8A038", ghost: "#705898", ice: "#98D8D8", dragon: "#7038F8",
+      dark: "#705848", steel: "#B8B8D0", flying: "#A890F0"
+    };
+
+
+    let offset = 0;
+  const limit = 10;
+  let pokemons = [];
+
+
+async function Init() {
+   await loadPokemon()
+    renderTest()
+}
+
+  async function loadPokemon() {
+    const apiUrl = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+    const res = await fetch(apiUrl);
+    const data = await res.json();
+
+    const details = await Promise.all(
+      data.results.map(p => fetch(p.url).then(r => r.json()))
+    );
+    pokemons = pokemons.concat(details);
+    offset += limit;
+    return pokemons
+  }
+
+function renderTest() {
+     for (let i = 0; i < pokemons.length; i++) {
+    console.log(pokemons[i].id);
+    
+    
+  }
+ }
+
---
+++ Js-ordner/template.js
@@ -0,0 +1,20 @@
+function templateRender(i) {
+    return`
+    <div class="card">
+    <div class="card-text-1">
+    <h5 class="card-title">#1</h5>
+    <h5 class="card-title">Bisasam</h5>
+    </div>
+    <div class="selector"></div>
+    <div class="poke-img-container">
+  <img src="..." class="card-img-top" alt="...">
+    </div>
+    <div class="selector"></div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">An item</li>
+    <li class="list-group-item">A second item</li>
+  </ul>
+</div>
+    
+    `
+}
